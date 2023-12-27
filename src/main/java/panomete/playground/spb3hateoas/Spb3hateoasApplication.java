b6c86302e07abf16a2b50e00e594ad1642e75b43
oat431 <oat431@gmail.com>
2023-12-28 00:21:14 +0700

    🛠 setup: project playground for hateoas

---
+++ src/main/java/panomete/playground/spb3hateoas/Spb3hateoasApplication.java
@@ -0,0 +1,13 @@
+package panomete.playground.spb3hateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spb3hateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spb3hateoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/panomete/playground/spb3hateoas/Spb3hateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package panomete.playground.spb3hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spb3hateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
